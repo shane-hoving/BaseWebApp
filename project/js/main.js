988c379928db92be9732a8e1e5fb59a389a63b71
hovingnutrition <79527892+hovingnutrition@users.noreply.github.com>
2021-04-09 13:40:28 -0500

    It's now a weather app.

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,22 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+/*$(document).ready(function(){
+  getWeather();
+})*/
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid=" + apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error has occured.");
+  }})
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
